de6f5bbcf2463ece7523dce2b23468c8b50ea74a
陈鹏 <simple19930611@gmail.com>
2019-11-15 17:15:15 +0800

    add:putExtra

--- library/src/main/java/com/simple/proxy/ARProxy.java
+++ library/src/main/java/com/simple/proxy/ARProxy.java
@@ -2,9 +2,16 @@ package com.simple.proxy;
 
 import android.content.ComponentName;
 import android.content.Intent;
+import android.os.Bundle;
+import android.os.IBinder;
+import android.os.Parcelable;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.FragmentActivity;
 import androidx.fragment.app.FragmentManager;
+
+import java.io.Serializable;
+import java.util.ArrayList;
 
 public class ARProxy {
 
@@ -25,13 +32,8 @@ public class ARProxy {
         return this;
     }
 
-    public ARProxy put(){
-
-        return this;
-    }
-
     public void getResult(int requestCode, OnResultListener listener) {
-        if (mIntent.getComponent() == null){
+        if (mIntent.getComponent() == null) {
             throw new NullPointerException("navTo method no call");
         }
 
@@ -47,5 +49,154 @@ public class ARProxy {
         manager.executePendingTransactions();
     }
 
+    //31 methods
+    public ARProxy putExtra(String name, boolean value) {
+        mIntent.putExtra(name, value);
+        return this;
+    }
 
+    public ARProxy putExtra(String name, byte value) {
+        mIntent.putExtra(name, value);
+        return this;
+    }
+
+    public ARProxy putExtra(String name, char value) {
+        mIntent.putExtra(name, value);
+        return this;
+    }
+
+    public ARProxy putExtra(String name, short value) {
+        mIntent.putExtra(name, value);
+        return this;
+    }
+
+    public ARProxy putExtra(String name, int value) {
+        mIntent.putExtra(name, value);
+        return this;
+    }
+
+    public ARProxy putExtra(String name, long value) {
+        mIntent.putExtra(name, value);
+        return this;
+    }
+
+    public ARProxy putExtra(String name, float value) {
+        mIntent.putExtra(name, value);
+        return this;
+    }
+
+    public ARProxy putExtra(String name, double value) {
+        mIntent.putExtra(name, value);
+        return this;
+    }
+
+    public ARProxy putExtra(String name, String value) {
+        mIntent.putExtra(name, value);
+        return this;
+    }
+
+    public ARProxy putExtra(String name, CharSequence value) {
+        mIntent.putExtra(name, value);
+        return this;
+    }
+
+    public ARProxy putExtra(String name, Parcelable value) {
+        mIntent.putExtra(name, value);
+        return this;
+    }
+
+    public ARProxy putExtra(String name, Parcelable[] value) {
+        mIntent.putExtra(name, value);
+        return this;
+    }
+
+    public ARProxy putParcelableArrayListExtra(String name, ArrayList<? extends Parcelable> value) {
+        mIntent.putParcelableArrayListExtra(name, value);
+        return this;
+    }
+
+    public ARProxy putIntegerArrayListExtra(String name, ArrayList<Integer> value) {
+        mIntent.putIntegerArrayListExtra(name, value);
+        return this;
+    }
+
+    public ARProxy putStringArrayListExtra(String name, ArrayList<String> value) {
+        mIntent.putStringArrayListExtra(name, value);
+        return this;
+    }
+
+    public ARProxy putCharSequenceArrayListExtra(String name, ArrayList<CharSequence> value) {
+        mIntent.putCharSequenceArrayListExtra(name, value);
+        return this;
+    }
+
+    public ARProxy putExtra(String name, Serializable value) {
+        mIntent.putExtra(name, value);
+        return this;
+    }
+
+    public ARProxy putExtra(String name, boolean[] value) {
+        mIntent.putExtra(name, value);
+        return this;
+    }
+
+    public ARProxy putExtra(String name, byte[] value) {
+        mIntent.putExtra(name, value);
+        return this;
+    }
+
+    public ARProxy putExtra(String name, short[] value) {
+        mIntent.putExtra(name, value);
+        return this;
+    }
+
+    public ARProxy putExtra(String name, char[] value) {
+        mIntent.putExtra(name, value);
+        return this;
+    }
+
+    public ARProxy putExtra(String name, int[] value) {
+        mIntent.putExtra(name, value);
+        return this;
+    }
+
+    public ARProxy putExtra(String name, long[] value) {
+        mIntent.putExtra(name, value);
+        return this;
+    }
+
+    public ARProxy putExtra(String name, float[] value) {
+        mIntent.putExtra(name, value);
+        return this;
+    }
+
+    public ARProxy putExtra(String name, double[] value) {
+        mIntent.putExtra(name, value);
+        return this;
+    }
+
+    public ARProxy putExtra(String name, String[] value) {
+        mIntent.putExtra(name, value);
+        return this;
+    }
+
+    public ARProxy putExtra(String name, CharSequence[] value) {
+        mIntent.putExtra(name, value);
+        return this;
+    }
+
+    public ARProxy putExtra(String name, Bundle value) {
+        mIntent.putExtra(name, value);
+        return this;
+    }
+
+    public ARProxy putExtras(@NonNull Intent src) {
+        mIntent.putExtras(src);
+        return this;
+    }
+
+    public ARProxy putExtras(@NonNull Bundle extras) {
+        mIntent.putExtras(extras);
+        return this;
+    }
 }
